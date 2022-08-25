e99b6043afecf422fe3873b5992033fbd5be01fb
blackeichi <87626378+blackeichi@users.noreply.github.com>
2022-08-25 22:12:54 +0900

    2022.08.25

--- navigators/OutNav.js
+++ navigators/OutNav.js
@@ -1,12 +1,25 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
+import { Text } from "react-native";
 import Begin from "../screens/Begin";
 
 const Nav = createNativeStackNavigator();
 
 const OutNav = () => (
-  <Nav.Navigator screenOptions={{ presentation: "modal", headerShown: false }}>
-    <Nav.Screen name="Begin" component={Begin} />
+  <Nav.Navigator screenOptions={{ presentation: "modal" }}>
+    <Nav.Screen
+      name="Begin"
+      options={{
+        title: "Watcha",
+        headerTitleAlign: "center",
+        headerRight: () => <Text>로그인</Text>,
+        headerTitleStyle: {
+          fontWeight: "800",
+        },
+        headerLargeTitle: true,
+      }}
+      component={Begin}
+    />
   </Nav.Navigator>
 );
 
--- screens/Begin.js
+++ screens/Begin.js
@@ -1,15 +1,25 @@
 import React from "react";
-import { View } from "react-native-web";
+import { Text, View } from "react-native";
 import styled from "styled-components/native";
 
-const Header = styled.View``;
-const Title = styled.Text``;
+const Header = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 15px 15px;
+`;
+const Title = styled.Text`
+  font-size: 24px;
+  font-weight: 800;
+`;
+const LoginBtn = styled.Text``;
 
 const Begin = () => (
   <>
     <Header>
-      <View />
       <Title>Watcha</Title>
+      <LoginBtn>로그인</LoginBtn>
     </Header>
   </>
 );
