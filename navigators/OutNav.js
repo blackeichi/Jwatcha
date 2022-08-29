bfe021cf875f9fd4d6c5a4dfc2911a1813f94bbe
blackeichi <87626378+blackeichi@users.noreply.github.com>
2022-08-29 12:13:05 +0900

    2022.08.29

--- navigators/OutNav.js
+++ navigators/OutNav.js
@@ -1,26 +1,40 @@
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import React from "react";
+import { useScroll } from "framer-motion";
+import React, { useEffect } from "react";
 import { Text } from "react-native";
 import Begin from "../screens/Begin";
 
 const Nav = createNativeStackNavigator();
-
-const OutNav = () => (
-  <Nav.Navigator screenOptions={{ presentation: "modal" }}>
-    <Nav.Screen
-      name="Begin"
-      options={{
-        title: "Watcha",
-        headerTitleAlign: "center",
-        headerRight: () => <Text>로그인</Text>,
-        headerTitleStyle: {
-          fontWeight: "800",
-        },
-        headerLargeTitle: true,
-      }}
-      component={Begin}
-    />
-  </Nav.Navigator>
-);
+const OutNav = () => {
+  const { scrollY } = useScroll();
+  useEffect(() => {
+    return scrollY.onChange((latest) => {
+      console.log(latest);
+    });
+  });
+  return (
+    <Nav.Navigator screenOptions={{ presentation: "modal" }}>
+      <Nav.Screen
+        name="Begin"
+        options={{
+          title: "Watcha",
+          headerTitleAlign: "center",
+          headerRight: () => (
+            <Text style={{ fontSize: 17, fontWeight: "800", color: "white" }}>
+              로그인
+            </Text>
+          ),
+          headerTitleStyle: {
+            fontWeight: "800",
+            fontSize: 26,
+          },
+          headerTintColor: "#FF0558",
+        }}
+        component={Begin}
+      />
+    </Nav.Navigator>
+  );
+};
 
 export default OutNav;
